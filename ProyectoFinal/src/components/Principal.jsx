6b3df40e1d6a6e393dad1a875ec2c76aace3a53c
nazyri <168209590+nazyri@users.noreply.github.com>
2024-09-14 10:10:09 -0600

    carousel

--- ProyectoFinal/src/components/Principal.jsx
+++ ProyectoFinal/src/components/Principal.jsx
@@ -1,5 +1,3 @@
-// src/components/Principal.js
-
 import '../styles/Principal.css';
 
 const Principal = () => {
@@ -9,41 +7,33 @@ const Principal = () => {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <div className='cards-wrapper'>
-              <div className="card" style={{ width: '18rem' }}>
+              <div className="card">
                 <img src="/src/img/WhatsApp Image 2024-09-13 at 1.37.39 PM (1).jpeg" className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">Card title</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                  <a href="#" className="btn btn-primary">Comprar</a>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <div className='cards-wrapper'>
-              <div className="card" style={{ width: '18rem' }}>
+              <div className="card">
+                <img src="/src/img/WhatsApp Image 2024-09-13 at 1.37.39 PM (2).jpeg" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                  <a href="#" className="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+              <div className="card">
                 <img src="/src/img/WhatsApp Image 2024-09-13 at 1.37.39 PM (3).jpeg" className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">Card title</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                  <a href="#" className="btn btn-primary">Comprar</a>
                 </div>
               </div>
             </div>
           </div>
-          <div className="carousel-item">
-            <div className='cards-wrapper'>
-              <div className="card" style={{ width: '18rem' }}>
-                <img src="/src/img/WhatsApp Image 2024-09-13 at 1.37.39 PM.jpeg" className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
           </div>
-        </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
           <span className="carousel-control-prev-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Previous</span>
@@ -58,3 +48,37 @@ const Principal = () => {
 };
 
 export default Principal;
+
+
+
+// <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+// <div className="carousel-inner">
+//   <div className="carousel-item active">
+//     <div className='cards-wrapper'>
+//   <div className="card">
+//       <img src="/src/img/WhatsApp Image 2024-09-13 at 1.37.39 PM (5).jpeg" className="card-img-top" alt="..." />
+//       <div className="card-body">
+//         <h5 className="card-title">Card title</h5>
+//         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+//         <a href="#" className="btn btn-primary">Comprar</a>
+//       </div>
+//   </div>
+//   <div className="card">
+//       <img src="/src/img/WhatsApp Image 2024-09-13 at 1.37.39 PM (4).jpeg" className="card-img-top" alt="..." />
+//       <div className="card-body">
+//         <h5 className="card-title">Card title</h5>
+//         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+//         <a href="#" className="btn btn-primary">Comprar</a>
+//       </div>
+//   </div>
+//   <div className="card">
+//       <img src="/src/img/WhatsApp Image 2024-09-13 at 1.37.38 PM.jpeg" className="card-img-top" alt="..." />
+//       <div className="card-body">
+//         <h5 className="card-title">Card title</h5>
+//         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+//         <a href="#" className="btn btn-primary">Comprar</a>
+//       </div>
+//   </div>
+// </div>
+// </div>
+// </div>
